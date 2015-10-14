1d1526a6a211237b2500a217c64081ffb581f1ee
shengyushen <shengyushen@gmail.com>
2015-10-14 11:45:54 +0800

    instating ssy_env

--- uvm/t1.sv
+++ uvm/t1.sv
@@ -14,17 +14,42 @@ initial begin
 end
 
 
+class ssy_env extends uvm_env;
+	`uvm_component_utils (ssy_env)
+
+	function new (string name = "ssy_env" , uvm_component parent = null);
+		//calling super.new must be the first statement 
+		//$display("ssy_env::new before suprt.new");
+		super.new (name,parent);
+		$display("ssy_env::new after super.new");
+	endfunction : new
+
+	virtual function void build_phase(uvm_phase phase);
+		$display("ssy_env::build_phase before super.build_phase");
+		super.build_phase(phase);
+		$display("ssy_env::build_phase after super.build_phase");
+	endfunction : build_phase
+endclass : ssy_env
+
 class ssy_test extends uvm_test;
 	`uvm_component_utils (ssy_test)
 
+	ssy_env inst_env;
+
 	function new (string name = "ssy_test", uvm_component parent = null);
+		//calling super.new must be the first statement 
+		//$display("ssy_test::new before super.new");
 		super.new (name,parent);
-		$display("ssy_test.new");
+		$display("ssy_test::new after super.new");
 	endfunction
 
 	virtual function void build_phase ( uvm_phase phase);
+		$display("ssy_test::build_phase before super.build_phase");
 		super.build_phase(phase);
-		$display("ssy_test.build_phase");
+		$display("ssy_test::build_phase after super.build_phase");
+		$display("ssy_test::build_phase before inst_env= create");
+		inst_env = ssy_env::type_id::create("inst_env",this);
+		$display("ssy_test::build_phase after inst_env= create");
 	endfunction
 endclass : ssy_test
 
