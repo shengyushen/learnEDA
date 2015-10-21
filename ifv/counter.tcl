72cbcef01968438ed3b8d125d5e02fce09787c3d
shengyushen <shengyushen@gmail.com>
2015-10-21 10:09:51 +0800

    modified tcl

--- ifv/counter.tcl
+++ ifv/counter.tcl
@@ -1,9 +1,15 @@
-ock -add clk -initial 0
+clock -add clk -initial 0
 force reset 0
 run 2
+
+# this init command already define the init state
 init -load -current
 init -show
+debug -init
+
+# these following commands are for setting prove
 constraint -add -pin reset 1
 assertion -add *
+#define delay_assertion 4
 prove
 
