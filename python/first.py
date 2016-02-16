41343b46e6080a42b050b8c242fbcb91bda9152f
shengyushen <shengyushen@gmail.com>
2016-02-16 16:53:15 +0800

    adding new code

--- python/first.py
+++ python/first.py
@@ -53,3 +53,89 @@ print xxx
 
 for i in range(1,3):
 	print i,xx[i]
+
+
+def ssy () :
+	"trying global"
+	print "global xx is ",xx
+	# as shown below, if I define xx =1, then xx will become a local 
+	# variable, which lead to an error in above global case
+	#xx=1
+	#print "local xx is ",xx
+
+ssy()
+
+f=ssy
+print "assign ssy to f , and call f"
+f();
+
+
+def fib(n):
+	"returning a fib array"
+	result=[]
+	a,b=0,1
+	while a<n :
+		result.append(a)
+		a,b=b,a+b
+	return result
+
+print fib(20)
+
+def ask (prompt,complain="haha"):
+	while True :
+		ok=raw_input (prompt)
+		if ok in ("y","yes"):
+			return True
+		elif ok in ("n","no"):
+			return False
+		else :
+			print complain
+
+#ask ("input ")
+
+# default parameter is evaled only once, 
+# so it may be different if you modify it
+def f1(a,L=[]):
+	L.append(a)
+	print L
+
+f1(1)
+f1(2)
+f1(3)
+
+print """
+working on tuple
+"""
+def parrot(k,d):
+	for ki in k:
+		print ki , d[ki]
+
+k=(1,2,3)
+d={1:2,2:3,3:4}
+parrot(k,d)
+
+print """
+working on lambda
+"""
+f=lambda x:x+1
+print "lambda 1+1 is ",f(1)
+
+
+f=[3,1,2,5]
+f.sort()
+print """ sorting 3 1 2 5 is"""
+print f
+
+f.sort(reverse=True)
+print """rev sorting 3 1 2 5 is"""
+print f
+
+print """working on queue with list that is fast"""
+from collections import deque
+que = deque([1,2,3])
+que.append(4)
+que.append(5)
+print que.popleft()
+print que.popleft()
+print que
+
